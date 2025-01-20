88a4d0fa4dd51339bcb122d6f6ae3e5e8ad0a736
avren <szymonurbanski217@gmail.com>
2025-01-20 07:12:56 +0100

    app routes fix

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const recipesRoutes = require('./routes/recipesRoutes');  // Add this line to import recipesRoutes
+
+const recipesRoutes = require('./routes/recipesRoutes');
+const goalsRoutes = require('./routes/goalsRoutes');
+const dashboardRoutes = require('./routes/dashboardRoutes');
 
 const app = express();
 
-// MongoDB connection
 mongoose
   .connect('mongodb://127.0.0.1:27017/calorieCounter', {
     useNewUrlParser: true,
@@ -16,25 +18,21 @@ mongoose
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
-// Middleware for handling form submissions
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Set up the view engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Serve static files (CSS, JS, etc.)
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Mount routes for recipes
-app.use('/recipes', recipesRoutes);  // This makes all recipes-related routes start with "/recipes"
+app.use('/dashboard', dashboardRoutes)
+app.use('/recipes', recipesRoutes); 
+app.use('/goals', goalsRoutes);
 
-// 404 error handler
 app.use((req, res) => {
   res.status(404).send('Page not found');
 });
 
-// Start the server
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
